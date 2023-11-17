48d8abf6dbd863af72fb0e9714b7aeeba8c44852
Filip Stojanovski <145137873+filipstojanovski25@users.noreply.github.com>
2023-11-17 16:46:19 +0100

    treta zadaca

--- inex.js
+++ inex.js
@@ -1,15 +1,41 @@
-function printFunc(seconds){
-    if (typeof seconds === "number") {
-        if (seconds >=0 && seconds <=9){
-            setTimeout(function(){
-                console.log("Coding is not that hard!")
-            },seconds * 1000)
-        } else {
-            console.log("Please enter a one digit number.")
-        }
+// function printFunc(seconds){
+//     if (typeof seconds === "number") {
+//         if (seconds >=0 && seconds <=9){
+//             setTimeout(function(){
+//                 console.log("Coding is not that hard!")
+//             },seconds * 1000)
+//         } else {
+//             console.log("Please enter a one digit number.")
+//         }
     
+//     } else {
+//         console.log("Please enter a number!")
+//     }  
+// }
+// printFunc(16)
+
+
+function checkNumber(number){
+  return new Promise((resolve,reject) => {
+    if(number % 2===0) {
+        resolve("Бројот е парен")
     } else {
-        console.log("Please enter a number!")
-    }  
+        reject(new Error("Бројо е непарен"))
+    }
+  })
 }
-printFunc(16)
+function handleSuccess(message){
+    console.log(message)
+}
+
+function handleError(error){
+    console.error(error.message)
+}
+
+checkNumber(4)
+    .then(handleSuccess) 
+    .catch(handleError);
+
+   checkNumber(9)
+   .then(handleSuccess)
+   .catch(handleError) 
